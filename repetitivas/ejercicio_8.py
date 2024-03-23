521c47c1bd004255e968dc7a50af2f7736ddd843
unknown <SENA@IBAAPRCLLFSP024.FormacionSena.RED>
2024-03-23 11:13:17 -0500

    cuarto comentario

--- repetitivas/ejercicio_8.py
+++ repetitivas/ejercicio_8.py
@@ -1,20 +1,22 @@
-import random
-valorCompra=int(input("se ingrasa el valor de la compra: "))
-aleatorio = random.choice(["con la valota roja", "cion la balota verde", "con la balota azul", "con la balota amarrilla", "con la balota negra"])
-if valorCompra>=50000 and balota=="rojo":
-    descuento=0.10
-    print(valorCompra*descuento)
-elif valorCompra>=50000 and balota=="verde":
-    descuento=0.15
-    print(valorCompra*descuento)
-elif valorCompra>=50000 and balota=="azul":
-    descuento=0.20
-    print(valorCompra*descuento)
-elif valorCompra>=50000 and balota=="amarilla":
-    descuento=0.50
-    print(valorCompra*descuento)
-elif valorCompra>=50000 and balota== "negra":
-    descuento=100
-    print(valorCompra*descuento)
-elif valorCompra<=50000:
-    print("no participas")
+import random 
+valorCompra=int(input("ingresa el valor de la compra: "))
+aleatorio = random.choice(["con la balota roja", "con la balota verde", "con la balota azul", "con la balota amarrilla", "con la balota negra"])
+
+print(aleatorio)
+if aleatorio == "con la balota roja":
+    balotaRoja = valorCompra*0.10 
+    print("El descuento total a pagar seria de: ",balotaRoja)
+elif aleatorio == "con balota verde":
+    balotaVerde = valorCompra*0.15
+    print("El descuento total a pagar seria de: ", balotaVerde)
+
+elif aleatorio == "con la balota azul":
+    balotaAzul = valorCompra * 0.20
+    print("El descuento total a pagar seria de: ", balotaAzul)
+elif aleatorio == "con la balota amarilla":
+    balotaAmarilla = valorCompra*0.50
+    print("el decuento a pagar seria de:", balotaAmarilla)
+elif aleatorio == "con la balota negra":
+    balotaNegra = valorCompra*1.0
+    print("el valor a pagar seria de: ", balotaNegra)
+
