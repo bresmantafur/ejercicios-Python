9818f7cb27d4f209ac9b8b9114e588cd5d5dca84
bresmantafur <145158339+bresmantafur@users.noreply.github.com>
2024-04-05 19:39:00 -0500

    quinto comentario

--- secuenciales/ejercicio_4.py
+++ secuenciales/ejercicio_4.py
@@ -1,10 +1,22 @@
-patos= int(input("digite el valor para la varible pato:"))
-dog= int(input("digite el valor prar la variable dog:"))
+dias_semana=[" lunes "," martes ", "miercoles", " jueves ", " viernes ", " sabado "]
+registro_estudiantes={}
+for dia in dias_semana:
+    cantidad_de_estudiantes= int(input(f"indrese la cantidad de estudiantes registrados para el dia{ dia }: "))
+    registro_estudiantes[dia] = cantidad_de_estudiantes
 
-numero=int(input("digite el numero:"))
+print("registro de estudiantes por dia:")
+for dia, cantidad in registro_estudiantes.items():
+    print(f"el numero de estudiantes que ingrsaron el dia {dia} es: {cantidad}" )
+total_estudiantes=sum(registro_estudiantes.values())
 partes=4
-ADSO=numero//partes
-print("el valor de las variable ADSO es:", ADSO)
-produccion_multimedia= numero//partes
-Desarrollo_de_videojuegos= numero//partes
-Sistema=numero//partes
+ADSO=total_estudiantes//partes
+print("El total de estudiates que ingresaron a la formacion de ADSO es:", ADSO)
+
+produccion_multimedia =total_estudiantes//partes
+print("El total de estudiates que ingresaron a la formacion de produccion Multimedia es:", produccion_multimedia)
+
+Desarrollo_de_videojuegos=total_estudiantes//partes
+print("El total de estudiates que ingresaron a la formacion de Videojuegos es:", Desarrollo_de_videojuegos)
+
+Sistemas=total_estudiantes//partes
+print("El total de estudiates que ingresaron a la formacion de sistemas es:", Sistemas)
--- selectivas/sistemas.py
+++ selectivas/sistemas.py
@@ -1,18 +1,39 @@
-"""El Centro de Industria y Comercio del Sena regional Tolima,quiere implementar un sistema de control de ingresos
-que permita contar los estudiantes que ingresan durante los seis dias de la semana 
-como tambien el número de estudiantes por formacion 
-Para el ejemplo vamos a tener en ceenta las siguientes formaciones:
-a. ADSO
-b. produccion multimedia 
-c. Desarrollo de videojuegos
-d. Sistemas
-El algoritmo debe indicar el total de estudiantes clasificados por cada programa, por dia"""
-estudiantes=0
-registro= int(input("ingrese el número de estudiates registrados: "))
-def dividir_registro (registro):
-    estudiantes=[0/4] *4
-    return estudiantes
-nuemero_total=0
-partes= dividir_registro("número_total")
+#permite saber el registro de los estudiantes
+registro_estudiantes={}
+contador=1
+while contador <= 6:
+    dia = ""
+    if contador == 1:
+        dia = "lunes"
+    elif contador == 2:
+        dia = "martes"
+    elif contador == 3:
+        dia = "miércoles"
+    elif contador == 4:
+        dia = "jueves"
+    elif contador == 5:
+        dia = "viernes"
+    elif contador == 6:
+        dia = "sábado"
 
-print ("registro" and "lunes") and ("el número de estudiantes por cada formacion es",estudiantes)
+    cantidad_de_estudiantes = int(input(f"Ingrese la cantidad de estudiantes registrados para el día {dia}: "))
+    registro_estudiantes[dia] = cantidad_de_estudiantes
+    contador += 1
+
+print("Registro de estudiantes por día:")
+for dia, cantidad in registro_estudiantes.items():
+    print(f"El número de estudiantes que ingresaron el día {dia} es: {cantidad}")
+
+total_estudiantes = sum(registro_estudiantes.values())
+partes = 4
+ADSO = total_estudiantes // partes
+print("El total de estudiantes que ingresaron a la formación de ADSO es:", ADSO)
+
+produccion_multimedia = total_estudiantes // partes
+print("El valor de la variable Producción Multimedia es:", produccion_multimedia)
+
+desarrollo_de_videojuegos = total_estudiantes // partes
+print("El valor de la variable Desarrollo de Videojuegos es:", desarrollo_de_videojuegos)
+
+sistemas = total_estudiantes // partes
+print("El valor de la variable Sistemas es:", sistemas)
