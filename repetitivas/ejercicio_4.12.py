024a50f46c436ab62250abeaf812264f14d68d93
bresmantafur <145158339+bresmantafur@users.noreply.github.com>
2024-05-11 12:44:33 -0500

    Delete repetitivas/ejercicio_4.12.py

--- repetitivas/ejercicio_4.12.py
+++
@@ -1,6 +0,0 @@
-"""Calcular e imprimir la tabla de multiplicar de un número cualquiera, el cual se
-digitará por teclado. Imprimir el multiplicando, el multiplicador y el producto."""
-numero= int(input("ingresar un numero para la tabla de multiplicar: "))
-for multiplicador in range(1, 11):
-    producto=numero*multiplicador
-    print(numero,"x", multiplicador, "=", producto)
