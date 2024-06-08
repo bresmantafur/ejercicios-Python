7d27b4d8b2e07de01bf2fb5146002e97de99aaa7
bresmantafur <145158339+bresmantafur@users.noreply.github.com>
2024-06-08 15:50:19 -0500

    Delete ejercicios-Python directory

--- ejercicios-Python/estructura de datos/ejercicio10.py
+++
@@ -1,8 +0,0 @@
-#. Escribir un programa que multiplique los 20 primeros números naturales
-
-resultado = 1
-
-for i in range(1, 21):
-    resultado *= i
-
-print("El resultado de la multiplicación de los primeros 20 números naturales es:", resultado)
